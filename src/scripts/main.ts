6a004e406e3676c8df8162d5fa2ea6873bdaa892
Jo <j.gugerbauer@gmail.com>
2024-07-04 23:36:56 +0200

    Add first files

---
+++ src/scripts/main.ts
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", initialise);
+
+function initialise() {
+    document.getElementById("bt_test")?.addEventListener("click", genericTest);
+}
+
+function genericTest() {
+    //alert("123");
+}
