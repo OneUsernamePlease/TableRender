48ec24a8829295036fda907cd13838e6bac2d65c
Jo <j.gugerbauer@gmail.com>
2024-07-05 18:48:33 +0200

    Feet-1: generate table, read inputs

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,23 +1,46 @@
 document.addEventListener("DOMContentLoaded", initialise);
 function initialise() {
-    generateTable("tableContainer");
+    var _a, _b;
+    (_a = document.getElementById("testBtn")) === null || _a === void 0 ? void 0 : _a.addEventListener("click", testfunction);
+    (_b = document.getElementById("btnGenerateTable")) === null || _b === void 0 ? void 0 : _b.addEventListener("click", regenerateTable);
+    generateTable();
 }
 //+++++
 var tableId = "mainTable";
 //+++++
-function generateTable(tableContainerId) {
-    removeTable(tableId);
-    var cellClassName = "pixel";
+function generateTable() {
     var table = document.createElement("table");
     var row = document.createElement("tr");
     var cell = document.createElement("td");
+    var cellClassName = "pixel";
+    var tableContainerId = "tableContainer";
+    var rowCnt = getInputNumber("tableHeightInput");
+    var colCnt = getInputNumber("tableWidthInput");
     table.setAttribute("id", tableId);
     cell.classList.add(cellClassName);
     row.appendChild(cell);
     table.appendChild(row);
     document.getElementById(tableContainerId).appendChild(table);
 }
+function regenerateTable() {
+    removeTable(tableId);
+    generateTable();
+}
 function removeTable(tableId) {
     var _a;
     (_a = document.getElementById(tableId)) === null || _a === void 0 ? void 0 : _a.remove();
+}
+function getInputString(inputId) {
+    var input;
+    input = document.getElementById(inputId);
+    return (!!input) ? input.value.trim() : "";
+}
+function getInputNumber(inputId) {
+    var inputValue = getInputString(inputId);
+    //because i will forget:
+    //+stringA converts stringA to number, if stringA is not numeric result = NaN, if it is numeric, result is stringA as number. "!isNan(+stringA)" is true if stringA is numeric, otherwise false
+    return (!isNaN(+inputValue)) ? +inputValue : 0;
+}
+function testfunction() {
+    removeTable(tableId);
 }
--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,20 +1,24 @@
 document.addEventListener("DOMContentLoaded", initialise);
 
 function initialise() {
-    generateTable("tableContainer")
+    document.getElementById!("testBtn")?.addEventListener("click", testfunction);
+    document.getElementById!("btnGenerateTable")?.addEventListener("click", regenerateTable);
+    generateTable();
 }
 
 //+++++
 let tableId = "mainTable";
 //+++++
 
-function generateTable(tableContainerId: string) {
-    removeTable(tableId);
-
+function generateTable() {
+    let table = document.createElement("table");
+    let row = document.createElement("tr");
+    let cell = document.createElement("td");
     let cellClassName = "pixel";
-    let table = document.createElement("table")
-    let row = document.createElement("tr")
-    let cell = document.createElement("td")
+    let tableContainerId = "tableContainer";
+    let rowCnt = getInputNumber("tableHeightInput");
+    let colCnt = getInputNumber("tableWidthInput");
+
     table.setAttribute("id", tableId);
     cell.classList.add(cellClassName);
     row.appendChild(cell);
@@ -22,7 +26,28 @@ function generateTable(tableContainerId: string) {
     document.getElementById(tableContainerId)!.appendChild(table);
 }
 
+function regenerateTable() {
+    removeTable(tableId);
+    generateTable();
+}
+
 function removeTable(tableId: string) {
     document.getElementById(tableId)?.remove();
 }
 
+function getInputString(inputId: string): string {
+    let input: HTMLElement | null;
+    input = document.getElementById(inputId);
+    return (!!input) ? (input as HTMLInputElement).value.trim() : "";
+}
+
+function getInputNumber(inputId: string): number {
+    let inputValue: string = getInputString(inputId);
+    //because i will forget:
+    //+stringA converts stringA to number, if stringA is not numeric result = NaN, if it is numeric, result is stringA as number. "!isNan(+stringA)" is true if stringA is numeric, otherwise false
+    return (!isNaN(+inputValue)) ? +inputValue : 0;
+}
+
+function testfunction() {
+    removeTable(tableId);
+}
