17199807be73beb45188d966b07931733c4f802e
Jo <j.gugerbauer@gmail.com>
2024-07-05 19:16:28 +0200

    Feet-1 generate table w/ size. add docu-comments

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -10,16 +10,22 @@ var tableId = "mainTable";
 //+++++
 function generateTable() {
     var table = document.createElement("table");
-    var row = document.createElement("tr");
-    var cell = document.createElement("td");
     var cellClassName = "pixel";
     var tableContainerId = "tableContainer";
     var rowCnt = getInputNumber("tableHeightInput");
     var colCnt = getInputNumber("tableWidthInput");
     table.setAttribute("id", tableId);
-    cell.classList.add(cellClassName);
-    row.appendChild(cell);
-    table.appendChild(row);
+    for (var i = 0; i < rowCnt; i++) {
+        var row = document.createElement("tr");
+        row.classList.add("r" + i);
+        for (var j = 0; j < colCnt; j++) {
+            var cell = document.createElement("td");
+            cell.classList.add(cellClassName);
+            cell.classList.add("c" + j);
+            row.appendChild(cell);
+        }
+        table.appendChild(row);
+    }
     document.getElementById(tableContainerId).appendChild(table);
 }
 function regenerateTable() {
@@ -31,11 +37,13 @@ function removeTable(tableId) {
     (_a = document.getElementById(tableId)) === null || _a === void 0 ? void 0 : _a.remove();
 }
 function getInputString(inputId) {
+    //return value of input element by id
     var input;
     input = document.getElementById(inputId);
     return (!!input) ? input.value.trim() : "";
 }
 function getInputNumber(inputId) {
+    //returns 0 if input's value is not numeric
     var inputValue = getInputString(inputId);
     //because i will forget:
     //+stringA converts stringA to number, if stringA is not numeric result = NaN, if it is numeric, result is stringA as number. "!isNan(+stringA)" is true if stringA is numeric, otherwise false
--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -12,17 +12,23 @@ let tableId = "mainTable";
 
 function generateTable() {
     let table = document.createElement("table");
-    let row = document.createElement("tr");
-    let cell = document.createElement("td");
     let cellClassName = "pixel";
     let tableContainerId = "tableContainer";
     let rowCnt = getInputNumber("tableHeightInput");
     let colCnt = getInputNumber("tableWidthInput");
 
     table.setAttribute("id", tableId);
-    cell.classList.add(cellClassName);
-    row.appendChild(cell);
-    table.appendChild(row);
+    for (let i = 0; i < rowCnt; i++) {
+        let row = document.createElement("tr");
+        row.classList.add("r" + i);
+        for(let j = 0; j < colCnt; j++) {
+            let cell = document.createElement("td");
+            cell.classList.add(cellClassName);
+            cell.classList.add("c" + j);
+            row.appendChild(cell);
+        }
+        table.appendChild(row);
+    }
     document.getElementById(tableContainerId)!.appendChild(table);
 }
 
@@ -36,12 +42,14 @@ function removeTable(tableId: string) {
 }
 
 function getInputString(inputId: string): string {
+    //return value of input element by id
     let input: HTMLElement | null;
     input = document.getElementById(inputId);
     return (!!input) ? (input as HTMLInputElement).value.trim() : "";
 }
 
 function getInputNumber(inputId: string): number {
+    //returns 0 if input's value is not numeric
     let inputValue: string = getInputString(inputId);
     //because i will forget:
     //+stringA converts stringA to number, if stringA is not numeric result = NaN, if it is numeric, result is stringA as number. "!isNan(+stringA)" is true if stringA is numeric, otherwise false
